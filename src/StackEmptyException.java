c556dd5a5e43fb80d42bd6d854f7633370e387b4
Meerab Wali <mwali@student.tgm.ac.at>
2025-02-19 10:37:16 +0100

    add full implementation of the Exception classes

--- src/StackEmptyException.java
+++ src/StackEmptyException.java
@@ -1,3 +1,19 @@
+/**
+ * Custom exception thrown when an operation is attempted on an empty stack.
+ * @author Meerab Wali
+ * @version 2024- 02-19
+ */
 public class StackEmptyException extends Exception{
-
+    /**
+     * Default constructor for StackEmptyException.
+     */
+    public StackEmptyException() {
+    }
+    /**
+     * Constructor that accepts a custom error message.
+     * @param message The error message describing the exception.
+     */
+    public StackEmptyException(String message) {
+        super(message);
+    }
 }
--- src/StackFullException.java
+++ src/StackFullException.java
@@ -1,3 +1,21 @@
+/**
+ * Custom exception thrown when an operation is attempted on a full stack.
+ * @author Meerab Wali
+ * @version 2024-02-19
+ */
 public class StackFullException extends Exception{
 
+    /**
+     * Default constructor for StackFullException.
+     */
+    public StackFullException() {
+    }
+
+    /**
+     * Constructor that accepts a custom error message.
+     * @param message The error message describing the exception.
+     */
+    public StackFullException(String message) {
+        super(message);
+    }
 }
