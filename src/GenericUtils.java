f5ff99979f8b22bfd1d1df2061efb6ab9c300bbc
Meerab Wali <mwali@student.tgm.ac.at>
2025-02-25 14:51:38 +0100

    add creation of needed classes

---
+++ src/GenericUtils.java
@@ -0,0 +1,14 @@
+public class GenericUtils <T> {
+
+    public void reverse (T[] array) {
+        int left = 0, right = array.length - 1;
+        while (left < right) {
+            T temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+}
